700998add8550304ae237b38dffab791ba0f5360
T0nia <edtoniaelohor@gmail.com>
2025-10-22 16:16:08 +0100

    Set up tasks API base with DRF - authentication required

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
